73624ffb3394681ff8c4863ce29764a883a99755
Austen Clement <austenincorporated@gmail.com>
2024-05-15 21:32:48 -0700

    progress

--- basicREST.py
+++ basicREST.py
@@ -6,7 +6,11 @@ requests.packages.urllib3.disable_warnings()
 
 # Initialize Array
 array = purestorage.FlashArray("10.235.116.230", api_token="26d6ca06-3496-6f9a-936e-5c88cd6ed359")
+array_info = array.get()
 
 # Validate Connection & Output Info
-array_info = array.get()
-print("FlashArray {} (version {}) REST session established!".format(array_info['array_name'], array_info['version']))
+def establish_session():
+    print("\nFlashArray {} (version {}) REST session established!\n".format(array_info['array_name'], array_info['version']))
+    print(array_info, "\n")
+
+establish_session()
